9d43eb8c058f8236e253419d77d479cb2b8493e3
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-10 17:52:28 -0400

    first commit

---
+++ StrechMyHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrechMyHeader
+//
+//  Created by Jaison Bhatti on 2017-10-10.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
